f38caaf5688a3386689c197f933a0ad9168e4c74
Andreas <andreasandreou1991@hotmail.com>
2015-05-02 17:00:46 +0300

    Listening for connections

--- epl371_ass4/server.c
+++ epl371_ass4/server.c
@@ -27,14 +27,51 @@ void printConfig();
 struct config conf;
 
 int main(int argc, char *argv[]) {
-	
+	int sock, serverlen;
+	struct sockaddr_in server;
+	struct sockaddr *serverptr;
+
 	setConfig();
 	printConfig();
+
+	// Create socket
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+	
+	server.sin_family = AF_INET; // Internet domain
+	server.sin_addr.s_addr = htonl(INADDR_ANY); // My Internet address
+	server.sin_port = htons(conf.port);
+	serverptr = (struct sockaddr *) &server;
+	serverlen = sizeof server;
+	
+	// Bind socket to an address
+	if (bind(sock, serverptr, serverlen) < 0) {
+		perror("bind");
+		exit(1);
+	}
+	
+	// Listen for connections
+	if (listen(sock, 5) < 0) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("Listening for connections to port %d\n", conf.port);
+	
+	while(1){
+		
+	}
+
 	return 0;
 }
 
-
-void setConfig(){
+/**
+ * Sets the configurations of the server base on the
+ * filename in 'FCONFIG' constant.
+ */
+void setConfig() {
 	FILE *file = fopen(FCONFIG, "r");
 
 	if (file != NULL) {
@@ -45,7 +82,7 @@ void setConfig(){
 			char *cfline;
 			cfline = strstr((char *) line, DELIM);
 			cfline = cfline + strlen(DELIM);
-			
+
 			if (i == 0) {
 				char temp_treads[MAXBUF];
 				memcpy(temp_treads, cfline, strlen(cfline));
@@ -61,9 +98,14 @@ void setConfig(){
 	fclose(file);
 }
 
-void printConfig(){
+/**
+ * Prints the configuration information of the server.
+ * The configuration includes: Number of threads and port number.
+ */
+void printConfig() {
 	printf("SERVER CONFIGURATION\n");
 	printf("====================\n");
 	printf("Number of Threads: %d\n", conf.num_treads);
 	printf("Port Number: %d\n\n", conf.port);
+	printf("-------------------------\n\n");
 }
