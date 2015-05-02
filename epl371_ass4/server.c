d1080da3fc85afa5a37fc0b456627699030ba759
Andreas <andreasandreou1991@hotmail.com>
2015-05-02 14:48:55 +0300

    testing hostname to ip

--- epl371_ass4/server.c
+++ epl371_ass4/server.c
@@ -1,13 +1,30 @@
-#include <stdio.h>
+#include <stdio.h> //printf
+#include <string.h> //strcpy
 #include <sys/socket.h>
+#include <netdb.h> //hostent
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-	int socket_desc;
-	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (socket_desc == -1) {
-		printf("Could not create socket");
+	char *hostname = "www.google.com";
+	char ip[100];
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+
+	if ((he = gethostbyname(hostname)) == NULL) {
+		//gethostbyname failed
+		herror("gethostbyname");
+		return 1;
 	}
-	
+
+	//Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+	addr_list = (struct in_addr **) he->h_addr_list;
+
+	for (i = 0; addr_list[i] != NULL; i++) {
+		//Return the first one;
+		strcpy(ip, inet_ntoa(*addr_list[i]));
+	}
+
+	printf("%s resolved to : %s", hostname, ip);
 	return 0;
 }
