914cb74c0e456fcfd5a2b82493b24658d8a2dbd3
Andreas <andreasandreou1991@hotmail.com>
2015-05-02 13:28:01 +0300

    ask4 given files

--- epl371_ass4/server.c
+++ epl371_ass4/server.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]) {
 	int socket_desc;
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-
+	
 	if (socket_desc == -1) {
 		printf("Could not create socket");
 	}
-
+	
 	return 0;
 }
